7aa27998e4e41053c0d59008a98c6668d010800b
Jonas Tophinke <jonas.tophine@hotmail.com>
2019-11-15 12:25:51 +0100

    Import

---
+++ Debug/Sources/drive/drive.d
@@ -0,0 +1,26 @@
+Sources/drive/drive.o: ../Sources/drive/drive.c ../Includes/platform.h \
+ ../Includes/MK22F51212.h ../Includes/core_cm4.h \
+ ../Includes/core_cmInstr.h ../Includes/core_cmFunc.h \
+ ../Includes/core_cmSimd.h ../Includes/system_MK22F51212.h \
+ ../Sources/drive/drive.h ../Sources/drive/quad.h \
+ ../Sources/drive/motor.h
+
+../Includes/platform.h:
+
+../Includes/MK22F51212.h:
+
+../Includes/core_cm4.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/core_cmSimd.h:
+
+../Includes/system_MK22F51212.h:
+
+../Sources/drive/drive.h:
+
+../Sources/drive/quad.h:
+
+../Sources/drive/motor.h:
---
+++ Debug/Sources/utils/subdir.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Sources/utils/ftm0.c \
+../Sources/utils/ftm3.c \
+../Sources/utils/led.c \
+../Sources/utils/util.c 
+
+OBJS += \
+./Sources/utils/ftm0.o \
+./Sources/utils/ftm3.o \
+./Sources/utils/led.o \
+./Sources/utils/util.o 
+
+C_DEPS += \
+./Sources/utils/ftm0.d \
+./Sources/utils/ftm3.d \
+./Sources/utils/led.d \
+./Sources/utils/util.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Sources/utils/%.o: ../Sources/utils/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -O0 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g3 -I"../Sources" -I"../Sources/com" -I"../Sources/drive" -I"../Sources/sound" -I"../Sources/utils" -I"../Includes" -std=c99 -Wa,-adhlns="$@.lst" -v -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
