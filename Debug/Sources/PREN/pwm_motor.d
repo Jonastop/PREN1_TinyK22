79935ed5a3d5d33673f0416260b772ac21133dfe
Riccardo Degiacomi <Riccardo Degiacomi@10.31.193.209>
2019-11-21 09:59:28 +0100

    irgend

---
+++ Debug/Sources/PREN/pwm_motor.d
@@ -0,0 +1,18 @@
+Sources/PREN/pwm_motor.o: ../Sources/PREN/pwm_motor.c \
+ ../Includes/platform.h ../Includes/MK22F51212.h ../Includes/core_cm4.h \
+ ../Includes/core_cmInstr.h ../Includes/core_cmFunc.h \
+ ../Includes/core_cmSimd.h ../Includes/system_MK22F51212.h
+
+../Includes/platform.h:
+
+../Includes/MK22F51212.h:
+
+../Includes/core_cm4.h:
+
+../Includes/core_cmInstr.h:
+
+../Includes/core_cmFunc.h:
+
+../Includes/core_cmSimd.h:
+
+../Includes/system_MK22F51212.h:
